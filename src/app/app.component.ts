89b2563f962901afd4c3417cd5dcd445617a1ed9
nal95 <alex.nguepi@yahoo.fr>
2024-05-06 17:39:17 +0200

    app logic and config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import {MatDrawer, MatDrawerContainer, MatDrawerContent} from "@angular/material/sidenav";
+import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {DataStoreService} from "./services/data-store/data-store.service";
+import {catchError, of, Subject, takeUntil} from "rxjs";
+import {HttpErrorResponse} from "@angular/common/http";
+import {DataService} from "./services/http-data/data.service";
+import {PreviewComponent} from "./components/preview/preview.component";
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, MatDrawerContainer, MatDrawer, MatDrawerContent, SidebarComponent, PreviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'cv-builder';
+  public unsubscribe$: Subject<void> = new Subject<void>();
+
+  constructor(private datasource: DataService,
+              private store: DataStoreService,
+              private matIconRegistry: MatIconRegistry,
+              private domSanitizer: DomSanitizer
+  ) {
+    this.addSvgIcons(['github-fill', 'global-fill', 'linkedin-box-fill', 'twitter-fill']);
+  }
+
+
+  ngOnInit(): void {
+    this.loadUserData();
+  }
+
+  loadUserData() {
+    this.datasource.getProfile()
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        catchError((error: HttpErrorResponse) => {
+          console.error('Error fetching user data:', error);
+          return of('An error occurred while fetching user data.');
+        })
+      )
+      .subscribe((data) => {
+        if (typeof data != 'string') {
+          this.store.setData(data);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  // TODO: this approach must be changed after, something happens in this methode
+  addSvgIcons(svgs: string[]) {
+    for (const svg of svgs) {
+      this.matIconRegistry.addSvgIcon(svg, this.domSanitizer
+        .bypassSecurityTrustResourceUrl("../assets/svgs/" + svg + ".svg"));
+    }
+  }
 }
