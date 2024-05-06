7774c2afd2be1d357229f1be51f1b829a92fde22
nal95 <alex.nguepi@yahoo.fr>
2024-05-06 22:22:09 +0200

    bug-fix: set them to Azure & Blue

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,6 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideAnimationsAsync(),
     provideAnimationsAsync(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(), provideAnimationsAsync()
   ]
 };
